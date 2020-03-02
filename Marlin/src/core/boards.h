13e04a756437086fa5c247c918591e642ca8e702
neuraldip <neuraldip@gmail.com>
2020-03-02 21:39:58 +0200

    Added Board Name

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -143,6 +143,7 @@
 #define BOARD_LEAPFROG_XEED2015       1321  // Leapfrog Xeed 2015
 #define BOARD_PICA_REVB               1322  // PICA Shield (original version)
 #define BOARD_PICA                    1323  // PICA Shield (rev C or later)
+#define BOARD_TENLOG_A                1324  // Tenlog Board. used also by D3 Hero
 
 //
 // ATmega1281, ATmega2561
